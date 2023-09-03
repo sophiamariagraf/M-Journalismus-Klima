22a869290c1fb94ae7224803d1205027966b1f6d
sophiamariagraf <sophia.graf@t-online.de>
2023-09-03 18:12:04 +0200

    Anfang Deskriptive Statistik Inferenzstatistik H2

---
+++ Deskriptive Statistik_Kommentare.R
@@ -0,0 +1,83 @@
+#Deskriptive Statistik zu den Kommentaren
+library('dplyr')
+install.packages('psych')
+library('psych')
+
+dataCom <- read.csv2("Journalismus&Klima_Kommentare_Mastersheet.csv")
+summary(dataCom)
+describe(dataCom)
+
+##Anzahl_Kommentare (SD, Schiefe, AM, Histogramm, Boxplott)
+### Mean: 281.49 
+### SD: 208.17 
+### min: 2 max: 719
+
+## Problematisierung
+### zur Kontrolle: --> alle (415) mit Problematisierung des Klimawandels
+table(dataCom$Problematisierung)
+
+## Lösungsansätze             16 414      3.36    1.09      4      3.57   0.00      1      4
+ComLösung <- table(dataCom$Lösungsansätze)
+print(ComLösung)
+rel_ComLösung <- round(prop.table(ComLösung), 3)
+print(rel_ComLösung)
+### A:   1: 0.12   2: 0.11   3: 0.05    4:0.72
+
+## Haltung_Lösungsansätze
+ComHaltungLösung <- table(dataCom$Haltung_Lösungsansätze)
+rel_ComHaltungLösung <- round(prop.table(ComHaltungLösung), 3)
+print(rel_ComHaltungLösung)
+
+Haltung_Lösung <- cbind(ComHaltungLösung, rel_ComHaltungLösung)
+
+## Haltung_Klimakrise
+ComHaltungKlima <- table(dataCom$Haltung_Klimakrise)
+rel_ComHaltungKlima <- round(prop.table(ComHaltungKlima), 3)
+print(rel_ComHaltungKlima)
+
+Haltung_Klima <- cbind(ComHaltungKlima, rel_ComHaltungKlima)
+round(Adressat_in, digits = 3)
+
+## Adressat_in
+ComAdress <- table(dataCom$Adressat_in)
+print(ComAdress)
+rel_ComAdress <- round(prop.table(ComAdress), 3)
+print(rel_ComAdress)
+
+Adressat_in <- cbind(ComAdress, rel_ComAdress)
+round(Adressat_in, digits = 3)
+
+### Kreisdiagramm
+
+
+## Lob
+ComLob <- table(dataCom$Lob)
+print(ComLob)
+rel_ComLob <- round(prop.table(ComLob), 3)
+print(rel_ComLob)
+
+Lob <- cbind(ComLob, rel_ComLob)
+
+## Kritik
+ComKritik <- table(dataCom$Kritik)
+print(ComKritik)
+rel_ComKritik <- round(prop.table(ComKritik), 3)
+print(rel_ComKritik)
+
+Kritik <- cbind(ComKritik, rel_ComKritik)
+
+## Beleidigung_Inzivilität
+ComBelInz <- table(dataCom$Beleidigung_Inzivilität)
+print(ComBelInz)
+rel_ComBelInz <- round(prop.table(ComBelInz), 3)
+print(rel_ComBelInz)
+
+Beleidigung_Inzivilität <- cbind(ComBelInz, rel_ComBelInz)
+
+# Kreuztabellen
+
+xtabLös <- table(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze)
+addmargins(round(prop.table(xtabLös, margin = 1), digits = 3))
+
+
+
---
+++ Inferenzstatistik_Kommentare.R
@@ -0,0 +1,72 @@
+# Inferenzstatistik Kommentare
+
+install.packages("rcompanion")
+library("rcompanion")
+
+install.packages("esc")
+library("esc")
+# H1: Die konstruktiven Elemente in den Artikeln haben keine Auswirkung 
+#### auf die Anzahl der Kommentare der Nutzer:innen.
+
+
+
+# H2: Die konstruktiven Elemente in den Artikeln sorgen dafür, dass 
+#### Lösungsansätze in den Kommentaren häufiger thematisiert werden.
+
+KreuztabLös <- xtabs(~ dataCom$Lösungsvorschlag + dataCom$Lösungsansätze)
+print(KreuztabLös)
+
+## Achtung wsl falsch/ mit bestehender Variable
+
+chisq.test(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze_JN) # X-squared = 105.72, df = 3, p-value < 2.2e-16
+fisher.test(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze) # p-value < 2.2e-16 alternative hypothesis: two.sided
+
+cohenW(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze) # Cohen w 0.5053 
+
+## Dichotome Variable Ja/ Nein
+
+library("dplyr")
+install.packages("sjmisc")
+library("sjmisc")
+
+DATEN_IA |> sjmisc::frq(Medium_Q)
+
+dataCom <- dataCom |>
+  mutate(Lösungsansätze_JN = rec(Lösungsansätze,
+                                 rec = "1,2,3 = 1 [JA]; 4 = 2 [NEIN]"))
+dataCom |> sjmisc::frq(Lösungsansätze_JN)
+
+## Kreuztabelle
+xtabLösJN <- xtabs(~ dataCom$Lösungsvorschlag + dataCom$Lösungsansätze_JN)
+print(xtabLösJN)
+
+## Chisq-Test
+
+chisq.test(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze_JN) # X-squared = 62.179, df = 1, p-value = 3.137e-15
+cohenW(dataCom$Lösungsvorschlag, dataCom$Lösungsansätze_JN) 
+
+esc_chisq(p = 3.137e-15, totaln = 413)
+esc_chisq(chisq = 62.179, totaln = 413)
+
+###Effect Size Calculation for Meta Analysis
+#Conversion: chi-squared-value to effect size d
+#Effect Size:   0.8420
+#Standard Error:   0.1068
+#Variance:   0.0114
+#Lower CI:   0.6327
+#Upper CI:   1.0513
+#Weight:  87.7053
+
+## alternative Packages? 
+install.packages("stats")
+library("stats")
+install.packages("vcd")
+library("vcd")
+
+cramerV(xtabLösJN) # Cramer V  0.3929  --> stimmt das ???
+
+# H3: Auf Artikel, die vorwiegend Mitigation als Lösungsansatz thematisieren (H3a) 
+#### und dabei die Bürger:innen adressieren (H3b), 
+#### reagieren die Nutzer:innen in den Kommentaren eher mit Kritik am Medium.
+
+# H4 
